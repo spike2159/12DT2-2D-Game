5a67e8953bc0eed0a66b97b0239e9f5fffc96a16
spike2159 <micaiah.rangi@gmail.com>
2024-06-07 10:55:00 +1200

    Fixing animations with basic movements
    Animations fixed so they now work for idle, left, right, fall, and jump.

--- Game project/scripts/character.gd
+++ Game project/scripts/character.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_falling = false
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,20 +17,22 @@ func _physics_process(delta):
 	# Handles jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("Jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions
-	# This is used for moving left or right and also plays the animation.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		if is_on_floor() and not Input.is_action_just_pressed("ui_accept"):
-			$AnimatedSprite2D.play("Run")
 		$AnimatedSprite2D.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor() and not Input.is_action_just_pressed("ui_accept"):
+
+	if velocity.y == 0 and is_on_floor():
+		if velocity.x == 0:
 			$AnimatedSprite2D.play("Idle")
+		if velocity.x < -1 or velocity.x > 1:
+			$AnimatedSprite2D.play("Run")
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play("Fall")
+	else:
+		$AnimatedSprite2D.play("Jump")
 
 	move_and_slide()
